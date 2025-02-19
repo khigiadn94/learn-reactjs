11678fa36f7860b04e6c59a37dce76c153b83264
unknown <nguyenkhanhdn94@gmail.com>
2025-02-19 03:23:50 -0500

    render element

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "khigiadn94";
+  const age = 17;
+  const isMale = true;
+  const student = {
+    name: "Haluhak",
+  };
+  const colorList = ["red", "green", "blue"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Haluhak-deploy-Vercel2</p>
+        <p>
+          Hello {name} - {age} - {isMale ? "Male" : "Female"}
+        </p>
+        {isMale ? <p>Male</p> : <p>Female</p>}
+        <ul>
+          {colorList.map((color) => (
+            <li key={color} style={{ color }}>
+              {color}
+            </li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
